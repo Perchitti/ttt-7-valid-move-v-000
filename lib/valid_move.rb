287dc919261831311e362cd382e3f7db3602235f
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-22 20:25:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,9 +4,7 @@
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken? (board, index)

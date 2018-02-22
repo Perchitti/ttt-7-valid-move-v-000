cab24eec1c8dafc7383f503cacf7f96c33d31b4c
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-22 19:10:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,3 +10,11 @@ def valid_move?(board, index)
     false
   end
 end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+end
+end

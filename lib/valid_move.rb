2920ec8ead3e8565dd241c616bdc4aeecc9f6903
Lauren Perchitti <laurenperchitti@gmail.com>
2018-02-22 20:06:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@
 def valid_move?(board, index)
   if index.between?(0,9)
     true
-  elsif board[index] == nil || board[index] > 9
+  if board[index] == nil || board[index] > 9
     false
   else
   end
